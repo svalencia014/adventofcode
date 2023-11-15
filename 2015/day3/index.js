c645cd61f38bc5e8da2e86d1311f6ef5adf71987
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-11-15 12:14:30 -0500

    ugh

--- 2015/day3/index.js
+++ 2015/day3/index.js
@@ -5,35 +5,39 @@ const input = fs.readFile('./input.txt', (error, data) => {
   if (error) { console.error(`Line 5: ${error}`); return;}
   let directions = data.toString().split('');
   for (let i = 0; i < directions.length; i++) {
-    if (directions[i] === '^') {
+    if (directions[i] == '^') {
       if (last == 'v') {
-        houses--;
+        return;
       } else {
         houses ++;
       }
       last = directions[i];
-    } else if (directions[i] === 'v') {
+    } else if (directions[i] == 'v') {
       if (last == '^') {
-        houses--;
+        return;
       } else {
         houses ++;
       }
       last = directions[i];
-    } else if (directions[i] === '<') {
+    } else if (directions[i] == '<') {
       if (last == '>') {
-        houses--;
+        return;
       } else {
         houses ++;
       }
       last = directions[i];
-    } else if (directions[i] === '>') {
+    } else if (directions[i] == '>') {
       if (last == '<') {
-        houses--;
+        return;
       } else {
         houses ++;
       }
       last = directions[i];
     }
+    if (i === directions.length - 1) {
+      houses--;
+    }
+    console.log(houses);
   }
   console.log(houses);
 })
---
+++ 2015/day4/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFile('./input.txt', (error, data) => {
+  if (error) { console.error(error); return; }
+  let correctHash = false;
+  for (let i = 0; !correctHash; i++) {
+    
+  }
+})
