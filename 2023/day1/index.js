694b5f4455d9abd697330083758086dbb844da92
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-12-05 13:45:31 -0500

    work on part 2

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 let total = 0;
 let linetotal = "";
+const numbers = {
+  "two": 2, 
+  "three": 3, 
+  "four": 4, 
+  "five": 5, 
+  "six": 6, 
+  "seven": 7, 
+  "eight": 8,
+  "nine": 9
+}
+
 fs.readFile('./input.txt', (error, data) => {
   if (error) console.error(error);
   lines = data.toString().split('\n');
   for (let i = 0; i < lines.length; i++) {
-    let chars = lines[i].match(/\d/g);
-    console.log(`Line ${i + 1} numbers: ${chars}`);
-    linetotal += chars[0] + chars[chars.length -1]
-    console.log(`Line ${i + 1} total: ${linetotal}`);
-    total += parseInt(linetotal);
-    linetotal = "";
-    console.log(`Calibration number is now: ${total}`);
+    let nums = lines[i].match(/\d+/g).split('');
+    console.log(`Line ${i + 1} chars: ${lines[i]}`);
+    console.log(`Line ${i + 1} nums: ${nums}`);
   }
 })
