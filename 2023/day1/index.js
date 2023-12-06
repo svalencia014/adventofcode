3dd975c13608856022249c3b37a1b8bf14ff620d
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2023-12-06 15:52:09 +0000

    change more things

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -17,7 +17,7 @@ fs.readFile('./input.txt', (error, data) => {
   if (error) console.error(error);
   lines = data.toString().split('\n');
   for (let i = 0; i < lines.length; i++) {
-    let chars = (lines[i].match(/((?:\b\d+|\bone|two|three|four|five|six|seven|eight|nine\b){1})/g).toString()).split(',')
+    let chars = (lines[i].match(/((?:\d{1}|one|two|three|four|five|six|seven|eight|nine){1})/g).toString()).split(',')
     if (chars[chars.length - 1] == 'null') {
       chars.pop()
     }
@@ -28,7 +28,13 @@ fs.readFile('./input.txt', (error, data) => {
     if (linetotal[1].length > 1) {
       linetotal[1] = numbers[linetotal[1]];
     }
+    linetotal = linetotal[0] + linetotal[1]
+    console.log(`Line ${i + 1} input: ${lines[i]}`)
     console.log(`Line ${i + 1} chars: ${chars}`);
     console.log(`Line ${i + 1} total: ${linetotal}`);
+    console.log('----------------------------------')
+    total += parseInt(linetotal);
+    linetotal = "";
   }
+  console.log(`The Calibration Number is ${total}`);
 })
