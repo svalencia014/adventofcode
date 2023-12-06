05ef3874273e490b0a930142c52f50158e8aebd2
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-12-05 22:08:52 -0500

    slow progress on part 2

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -2,22 +2,33 @@ const fs = require('fs');
 let total = 0;
 let linetotal = "";
 const numbers = {
-  "two": 2, 
-  "three": 3, 
-  "four": 4, 
-  "five": 5, 
-  "six": 6, 
-  "seven": 7, 
-  "eight": 8,
-  "nine": 9
+  "one": '1',
+  "two": '2', 
+  "three": '3', 
+  "four": '4', 
+  "five": '5', 
+  "six": '6', 
+  "seven": '7', 
+  "eight": '8',
+  "nine": '9'
 }
 
 fs.readFile('./input.txt', (error, data) => {
   if (error) console.error(error);
   lines = data.toString().split('\n');
   for (let i = 0; i < lines.length; i++) {
-    let nums = lines[i].match(/\d+/g).split('');
-    console.log(`Line ${i + 1} chars: ${lines[i]}`);
-    console.log(`Line ${i + 1} nums: ${nums}`);
+    let chars = (lines[i].match(/((?:\b\d+|\bone|two|three|four|five|six|seven|eight|nine\b)+)/g) + ',' + lines[i].match(/\d/g)).split(',');
+    if (chars[chars.length - 1] == 'null') {
+      chars.pop()
+    }
+    linetotal = [chars[0], chars[chars.length - 1]]
+    if (linetotal[0].length > 1) {
+      linetotal[0] = numbers[linetotal[0]];
+    }
+    if (linetotal[1].length > 1) {
+      linetotal[1] = numbers[linetotal[1]];
+    }
+    console.log(`Line ${i + 1} chars: ${chars}`);
+    console.log(`Line ${i + 1} total: ${linetotal}`);
   }
 })
