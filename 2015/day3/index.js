597714f9108b4a3404f4f5cee19ab257987dcd5b
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-04-03 09:26:56 -0400

    finish day 3

--- 2015/day3/index.js
+++ 2015/day3/index.js
@@ -1,44 +1,66 @@
 const fs = require('fs');
-let x = 0;
-let y = 0;
-let last = false;
-let visited = [];
-let houses = 0;
 fs.readFile('./input.txt', (error, data) => {
   if (error) { console.error(`Line 5: ${error}`); return;}
-  let directions = data.toString().split('');
-  for (let i = 0; i < directions.length; i++) {
-    switch(directions[i]) {
-      case '^': {
-        y++
-        break;
-      };
-      case 'v': {
-        y--;
-        break;
+  let instructions = data.toString().split('');
+  // Define starting positions for Santa and Robot Santa
+  let santaX = 0;
+  let santaY = 0;
+  let robotSantaX = 0;
+  let robotSantaY = 0;
+  
+  // Keep track of whose turn it is (true - Santa, false - Robot Santa)
+  let santasTurn = true;
+  
+  // Keep track of visited houses (Set to prevent duplicates)
+  let visited = [];
+  visited.push([0, 0]); //Starting position
+
+  for (let instruction of instructions) {
+    // Move Santa or Robot Santa based on whose turn it is
+    if (santasTurn) {
+      switch(instruction) {
+        case '^':
+          santaY++;
+          break;
+        case 'v':
+          santaY--;
+          break;
+        case '>':
+          santaX++;
+          break;
+        case '<':
+          santaX--;
+          break;
       }
-      case '>': {
-        x++;
-        break;
+      visited.push([santaX, santaY]);
+      santasTurn = false;
+    } else {
+      switch(instruction) {
+        case '^':
+          robotSantaY++;
+          break;
+        case 'v':
+          robotSantaY--;
+          break;
+        case '>':
+          robotSantaX++;
+          break;
+        case '<':
+          robotSantaX--;
+          break;
       }
-      case '<': {
-        x--;
-        break;
-      }
+      visited.push([robotSantaX, robotSantaY]);
+      santasTurn = true;
+      console.log(visited);
     }
-    for (let j = 0; j < visited.length; j++) {
-      if (visited[j][0] == x && visited[j][1] == y) {
-        last = true;
-        break;
-      } 
-    }
-    if (last != true) {
-      houses++
-    }
-    last = false;
-    visited.push([x,y]);
-    console.log(`${i/directions.length * 100}% complete`)
+    console.log(santaX, santaY, robotSantaX, robotSantaY)
   }
-  console.log(`Final coordinates: ${[x,y]}`);
-  console.log(`Visted ${houses} houses`);
+
+  // Print the final positions of Santa and Robot Santa
+  console.log(`Santa: (${santaX}, ${santaY})`);
+  console.log(`Robot Santa: (${robotSantaX}, ${robotSantaY})`);
+  
+  //Print the number of houses visited
+  let arr = visited.map(JSON.stringify).filter((e,i,a) => i === a.indexOf(e)).map(JSON.parse);
+  console.log(`Total houses visited: ${arr.length}`);
 })
