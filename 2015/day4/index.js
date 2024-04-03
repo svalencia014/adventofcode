bb86ca7ca2e687e5b200a4de3d4205cb41e9774a
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-04-03 10:23:04 -0400

    day 4 was ez

--- 2015/day4/index.js
+++ 2015/day4/index.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
+const md5 = require('./md5');
 const input = fs.readFile('./input.txt', (error, data) => {
   if (error) { console.error(error); return; }
   let correctHash = false;
-  for (let i = 0; !correctHash; i++) {
-    
+  let hash = ""
+  let input = data;
+  let number = 0;
+  for (let i = 1; correctHash == false; i++) {
+    input = data + i;
+    hash = md5(input);
+    if (hash.toString().startsWith("00000")) {
+      number = i;
+      correctHash = true;
+    }
   }
+  console.log(`The hash for part one is: ${hash}, which has the number ${number} appended to the input.`);
+  correctHash = false;
+  for (let i = 1; correctHash == false; i++) {
+    input = data + i;
+    hash = md5(input);
+    if (hash.toString().startsWith("000000")) {
+      number = i;
+      correctHash = true;
+    }
+  }
+  console.log(`The hash for part two is: ${hash}, which has the number ${number} appended to the input.`);
 })
